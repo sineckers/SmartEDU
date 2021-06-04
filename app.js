197f60a31d20a0b3e3150b74ae89ce86c69e1e83
SinecKers <yasar.ylmz.yt@gmail.com>
2021-06-04 17:58:00 +0300

    lesson4

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+
 const pageRoute = require('./routes/pageRoute.js');
+const courseRoute = require('./routes/courseRoute.js');
+
+mongoose.connect(process.env.MONGOURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true
+}).then(() => {
+  console.log("Database connection succesful.")
+})
 
 const app = express();
 
@@ -11,8 +25,9 @@ app.use(express.static('public'));
 
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
