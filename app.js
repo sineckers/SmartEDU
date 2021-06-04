298b0c8b5a312dda9a58c240a916a475efcc7ebb
SinecKers <yasar.ylmz.yt@gmail.com>
2021-06-04 18:29:11 +0300

    ders9

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute.js');
 const courseRoute = require('./routes/courseRoute.js');
 const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 mongoose.connect(process.env.MONGOURL, {
   useNewUrlParser: true,
@@ -30,6 +31,7 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
