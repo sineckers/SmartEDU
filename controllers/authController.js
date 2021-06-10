7004acef1a0dbc6646c32b413da96a945ab772c7
SinecKers <yasar.ylmz.yt@gmail.com>
2021-06-10 19:43:30 +0300

    ders18-19

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,11 +24,9 @@ exports.loginUser = async (req, res) => {
     await User.findOne({ email }, (err, user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
-          if (same) {
             // USER SESSION
             req.session.userID = user._id;
             res.status(200).redirect('/users/dashboard');
-          }
         });
       }
     });
@@ -47,7 +45,7 @@ exports.logoutUser = (req, res) => {
   }
 
   exports.getDashboardPage = async (req, res) => {
-    const user = await User.findOne({_id:req.session.userID});
+    const user = await User.findOne({_id:req.session.userID}).populate('courses');
     const categories = await Category.find();
     const courses = await Course.find({user:req.session.userID})
 
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.route('/').post(roleMiddleware(["teacher", "admin"]), courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/enroll').post(courseController.enrollCourse);
+router.route('/release').post(courseController.releaseCourse);
 
 module.exports = router;
