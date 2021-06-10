2f39be90e5257c4d0eec06cc02271169b396e6f4
SinecKers <yasar.ylmz.yt@gmail.com>
2021-06-10 20:22:08 +0300

    ders21

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,8 +1,54 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
     res.status(200).render('index', {
       page_name: 'index',
     });
+  };
+
+  exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+      page_name: 'contact',
+    });
+  };
+  
+  exports.sendEmail = async (req, res) => {
+  
+    const outputMessage = `
+    
+    <h1>Mail Details </h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `
+  
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "arinyazilim@gmail.com",
+        pass: "bpwrtssmqdsdjdjw",
+      },
+    });
+  
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <arinyazilim@gmail.com>',
+      to: "yasar.ylmz.yt@gmail.com",
+      subject: "Smart EDU Contact Form New Message ✔",
+      html: outputMessage
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  
+    res.status(200).redirect('contact');
+  
   };
   
   exports.getAboutPage = (req, res) => {
